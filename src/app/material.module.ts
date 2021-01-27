5037275def576f26fbca3a5a430dc2acc452522a
ranasolh <ranasolh1@hotmail.com>
2021-01-27 14:01:17 +0200

    make

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -6,27 +6,17 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 
 import {MatSelectModule} from '@angular/material/select';
 
-import {MatStepperModule} from '@angular/material/stepper';
 
-import {MatIconModule} from '@angular/material/icon';
-
-import {MatTableModule} from '@angular/material/table';
-import {MatSortModule} from '@angular/material/sort';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatButtonModule} from '@angular/material/button'
 
 
 @NgModule({
   exports: [
-    MatTableModule,
-    MatStepperModule,
-    MatButtonModule,
+    
+   
+   
     MatFormFieldModule,
     MatInputModule,
-    MatIconModule,
     MatSelectModule,
-    MatPaginatorModule,
-    MatSortModule,
     MatAutocompleteModule
    
   ]
